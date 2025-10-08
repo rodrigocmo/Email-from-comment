0a18e3e6fab761e160034a6126cac5a99c0e9a45
Rodrigo Morais <rcastromorais01@gmail.com>
2025-10-08 14:44:22 -0300

    tratamento de erro inicial + forma de envio sincrona

--- src/main/java/org/example/entity/dto/CommentCreatedEvent.java
+++ src/main/java/org/example/entity/dto/CommentCreatedEvent.java
@@ -4,8 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.example.entity.Comment;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
 @NoArgsConstructor
@@ -14,6 +16,8 @@ import java.time.LocalDateTime;
 public class CommentCreatedEvent {
     private Long commentId;
     private Long postId;
+    private Long userId;
+    private String eventId;
     private String postTitle;
     private String postAuthorEmail;
     private String postAuthorName;
@@ -21,4 +25,16 @@ public class CommentCreatedEvent {
     private String commentContent;
     private LocalDateTime createdAt;
     private Boolean emailNotificationsEnabled;
+
+
+    public static CommentCreatedEvent create(Comment comment) {
+        return CommentCreatedEvent.builder()
+                .commentId(comment.getId())
+                .eventId(UUID.randomUUID().toString())
+                .createdAt(LocalDateTime.now())
+                .postId(comment.getPostId())
+                .userId(comment.getAuthorId())
+                .commentContent(comment.getContent())
+                .build();
+    }
 }
--- src/main/java/org/example/service/KafkaProducerService.java
+++ src/main/java/org/example/service/KafkaProducerService.java
@@ -1,10 +1,19 @@
 package org.example.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.example.entity.Comment;
 import org.example.entity.dto.CommentCreatedEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Slf4j
@@ -22,4 +31,54 @@ public class KafkaProducerService {
         kafkaTemplate.send(TOPIC, event.getCommentId().toString(), event);
 
     }
+
+    public void publishCommentCreated(Comment comment) {
+        CommentCreatedEvent event = CommentCreatedEvent.create(comment);
+
+        // ========== PARTICIONAMENTO PARA GARANTIR ORDEM ==========
+        // Usa postId como chave para garantir que comentários do mesmo post
+        // vão sempre para a mesma partição (mantendo ordem)
+        String partitionKey = comment.getPostId().toString();
+
+        try {
+            // usar interface future para manter async
+            CompletableFuture<SendResult<String, CommentCreatedEvent>> future =
+                    kafkaTemplate.send("comment-created", partitionKey, event);
+
+            future.whenComplete((result, erro) -> {
+                if (Objects.nonNull(erro)) {
+                    log.error("Erro ao enviar mensagem para Kafka: {}", erro.getMessage(), erro);
+                } else {
+                    RecordMetadata metadata = result.getRecordMetadata();
+                    log.info("Mensagem enviada com sucesso - Topic: {}, Partition: {}, Offset: {}",
+                            metadata.topic(),
+                            metadata.partition(),
+                            metadata.offset());
+                }
+            }); // caso nao queira usar biconsumer pode usar thenaccept e exceptionally
+
+        } catch (Exception e) {
+            log.error("Erro inesperado ao enviar evento: commentId={}", comment.getId(), e);
+            throw new RuntimeException("Falha ao publicar evento", e);
+        }
+    }
+
+
+    public void publishCommentCreatedSync(Comment comment) {
+        CommentCreatedEvent event = CommentCreatedEvent.create(comment);
+
+        // Envio SÍNCRONO - bloqueia até confirmar entrega
+        try {
+            SendResult<String, CommentCreatedEvent> result = kafkaTemplate
+                    .send("comment-created", comment.getPostId().toString(), event)
+                    .get(10, TimeUnit.SECONDS); // get prende a thread até a conclusao ou timeout
+
+            log.info("Evento confirmado sincronamente: offset={}",
+                    result.getRecordMetadata().offset());
+        }catch (Exception e){
+            log.error("Erro inesperado ao enviar evento: commentId={}", comment.getId(), e);
+        }
+    }
+
+
 }
